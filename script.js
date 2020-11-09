2dc87bfea830f170cc96feec038e14681f5015f5
Nicholas Werner <ncwerner@gmail.com>
2020-11-09 10:13:46 -0700

    created js/ajax function that searches the current weather of the searched city

---
+++ script.js
@@ -0,0 +1,48 @@
+// $.ajax({
+//     url: "http://www.omdbapi.com/?t=joker&apikey=trilogy",
+//     method: "GET",
+// }).then(function (response) {
+//     console.log(response);
+// })
+
+// console.log("line 8 will console log before line 5");
+
+// $.ajax({
+//     url: "http://www.omdbapi.com/?t=titanic&apikey=trilogy",
+//     method: "GET",
+// }).then(function (response) {
+//     console.log(response);
+
+//     $.ajax({
+//         url: "http://www.omdbapi.com/?t=the+dark+knight&apikey=trilogy",
+//         method: "GET",
+//     }).then(function (response) {
+//         console.log(response);
+    
+//         /// get the response.Title and use dom manipulation to display onto the page
+//         $("#cityName").text(response.Title);
+//     })
+// })
+
+// console.log("line 17");
+
+// function to find searched city and current weather
+function searchWeather(city) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=44558b94b80d15a7785e0eb77b5845ac";
+    
+    $.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function (responce) {
+    console.log(responce)
+    
+})
+}
+
+$("#search").on("click", function(event) {
+    event.preventDefault();
+    var inputCity = $("#searchInput").val()
+    console.log(inputCity)
+    searchWeather(inputCity);
+});
+
